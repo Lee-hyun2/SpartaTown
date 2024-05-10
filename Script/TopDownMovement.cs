f1b0007154686b6f103402b38a96432493a3f86b
Lee-hyun2 <167046397+Lee-hyun2@users.noreply.github.com>
2024-05-11 02:01:44 +0900

    [feat] Creating a character and moving it
    캐릭터 만들고 이동하기

---
+++ Script/TopDownMovement.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    // 실제로 이동이 일어날 컴포넌트
+
+    private TopDownController controller;
+    private Rigidbody2D movementRigidbody;
+    private Vector2 movementDirection = Vector2.zero;
+
+    private void Awake()
+    {
+        // 주로 내 컴포넌트 안에서 끝남
+
+        // controller랑 TopDownController랑 같은 게임 오브젝트 안에 있다라는 가정
+        controller = GetComponent<TopDownController>();
+        movementRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        movementDirection = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        // FixedUpdate는 물리업데이트 관련
+        // rigidbody 값을 비우니까 FixedUpdate
+        ApplyMovement(movementDirection);
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction = direction * 5;
+        movementRigidbody.velocity = direction;
+    }
+}
